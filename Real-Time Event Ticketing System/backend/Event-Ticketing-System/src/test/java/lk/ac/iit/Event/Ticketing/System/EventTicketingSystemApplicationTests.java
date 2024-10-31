2fef34f847c99ab4e554c6b2a201b64392ae957b
mnzralee <133229508+mnzralee@users.noreply.github.com>
2024-10-31 12:28:28 +0530

    Initialized Springboot Backend

---
+++ Real-Time Event Ticketing System/backend/Event-Ticketing-System/src/test/java/lk/ac/iit/Event/Ticketing/System/EventTicketingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ac.iit.Event.Ticketing.System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventTicketingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
