c729774161fff12e0566e16e197bb45a7d386ea8
Manazir Ali <133229508+mnzralee@users.noreply.github.com>
2024-12-16 17:09:17 +0530

    Delete JavaDoc directory

--- JavaDoc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"lk.ac.iit.backend"},{"l":"lk.ac.iit.backend.controller"},{"l":"lk.ac.iit.backend.model"},{"l":"lk.ac.iit.backend.repository"},{"l":"lk.ac.iit.backend.runnable"},{"l":"lk.ac.iit.backend.service"},{"l":"lk.ac.iit.backend.websocket"}];updateSearchResults();
